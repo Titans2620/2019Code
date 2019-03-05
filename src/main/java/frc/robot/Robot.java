09cb88effdaaeaad20ad7e7c057d63b16877aaaf
Titans2620 <35977107+Titans2620@users.noreply.github.com>
2019-03-05 15:02:57 -0500

    Robot Code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -27,24 +27,23 @@ public class Robot extends TimedRobot {
     /*************************************************************
                         Motor Controllers
     *************************************************************/
-    WPI_TalonSRX bottomRightDrive = new WPI_TalonSRX(1);
-    WPI_VictorSPX bottomLeftDrive = new WPI_VictorSPX(2);
-    WPI_VictorSPX topRightDrive = new WPI_VictorSPX(3);
-    WPI_TalonSRX topLeftDrive = new WPI_TalonSRX(4);
-    WPI_TalonSRX lift = new WPI_TalonSRX(5);
-    WPI_TalonSRX crossSlide = new WPI_TalonSRX(6);
-    //WPI_TalonSRX rLift = new WPI_TalonSRX(7);
-    WPI_TalonSRX lIntake = new WPI_TalonSRX(8);
-    WPI_TalonSRX rIntake = new WPI_TalonSRX(9);
-    WPI_TalonSRX frontClimb = new WPI_TalonSRX(10);
-    WPI_TalonSRX rearClimb = new WPI_TalonSRX(11);
-    WPI_VictorSPX intakeArm = new WPI_VictorSPX(14);
-    WPI_VictorSPX fifthteen = new WPI_VictorSPX(15);
-
     
-
-    WPI_TalonSRX rRearArmDrive = new WPI_TalonSRX(12);
-    WPI_TalonSRX lRearArmDrive = new WPI_TalonSRX(13);
+    
+    WPI_VictorSPX bottomLeftDrive = new WPI_VictorSPX(2);
+    WPI_TalonSRX topLeftDrive = new WPI_TalonSRX(1);
+    WPI_VictorSPX topRightDrive = new WPI_VictorSPX(3);
+    WPI_TalonSRX bottomRightDrive = new WPI_TalonSRX(4);
+    WPI_TalonSRX frontClimb2 = new WPI_TalonSRX(5);
+    WPI_TalonSRX frontClimb = new WPI_TalonSRX(6);
+    WPI_TalonSRX rRearArmDrive = new WPI_TalonSRX(7);
+    WPI_TalonSRX lRearArmDrive = new WPI_TalonSRX(8);
+    WPI_TalonSRX rearClimb = new WPI_TalonSRX(9);
+    WPI_TalonSRX rearClimb2 = new WPI_TalonSRX(10);
+    WPI_TalonSRX lift = new WPI_TalonSRX(11);
+    // WPI_TalonSRX rLift = new WPI_TalonSRX(12);
+    WPI_TalonSRX crossSlide = new WPI_TalonSRX(13);
+    WPI_VictorSPX intakeArm = new WPI_VictorSPX(14);
+    WPI_VictorSPX intake = new WPI_VictorSPX(15);
 
     
 
@@ -55,14 +54,18 @@ public class Robot extends TimedRobot {
     ************************************************************/
     DigitalInput rSlideSensor = new DigitalInput(0);
     DigitalInput lSlideSensor = new DigitalInput(1);
-    DigitalInput cSlideSensor = new DigitalInput(2);
-    DigitalInput rearClimbStop = new DigitalInput(3);
-    DigitalInput frontClimbStop = new DigitalInput(4);
+    //DigitalInput cSlideSensor = new DigitalInput(2);
+    DigitalInput bottomLift = new DigitalInput(2);
+    DigitalInput frontClimbStop = new DigitalInput(3);
+    DigitalInput rearClimbStop = new DigitalInput(4);
     DigitalInput zeroArm = new DigitalInput(5);
     Encoder armEnc = new Encoder(6,7,false, Encoder.EncodingType.k4X);
     DigitalInput ballSwitch = new DigitalInput(8);
+    DigitalInput topLift = new DigitalInput(9);
+    
     
 
+    
     
   
     /***********************************************************
@@ -71,7 +74,6 @@ public class Robot extends TimedRobot {
     SpeedControllerGroup rightDrive = new SpeedControllerGroup(bottomRightDrive, topRightDrive, rRearArmDrive);
     SpeedControllerGroup leftDrive = new SpeedControllerGroup(bottomLeftDrive, topLeftDrive, lRearArmDrive);
     DifferentialDrive diffDrive = new DifferentialDrive(leftDrive, rightDrive);
-    
     
 
     Joystick right = new Joystick(1);
@@ -115,10 +117,15 @@ public class Robot extends TimedRobot {
 
         
 
-        topLeftDrive.setInverted(true);
+        topRightDrive.setInverted(true);
         bottomRightDrive.setInverted(true);
         lRearArmDrive.setInverted(true);
-        
+        intake.setInverted(true);
+        frontClimb.setInverted(true);
+        rearClimb.setInverted(true);
+        frontClimb2.setInverted(true);
+        rearClimb2.setInverted(true);
+        lift.setInverted(true);
     }
  
     @Override
@@ -155,7 +162,7 @@ public class Robot extends TimedRobot {
     @Override
     public void teleopPeriodic()
     {
-       diffDrive.arcadeDrive(right.getRawAxis(1), right.getRawAxis(0) * -1);
+       diffDrive.arcadeDrive(right.getRawAxis(1), right.getRawAxis(0));
 
         boolean trigger = right.getRawButton(1);
         SmartDashboard.putBoolean("trigger", trigger);
@@ -179,18 +186,8 @@ public class Robot extends TimedRobot {
         boolean R4 = right.getRawButton(14);
         boolean R5 = right.getRawButton(15);
         boolean R6 = right.getRawButton(16);
-        SmartDashboard.putBoolean("L1", L1);
-        SmartDashboard.putBoolean("L3", L3);
-        SmartDashboard.putBoolean("L2", L2);
-        SmartDashboard.putBoolean("L4", L4);
-        SmartDashboard.putBoolean("L5", L5);
-        SmartDashboard.putBoolean("L6", L6);
-        SmartDashboard.putBoolean("R1", R1);
-        SmartDashboard.putBoolean("R2", R2);
-        SmartDashboard.putBoolean("R3", R3);
-        SmartDashboard.putBoolean("R4", R4);
-        SmartDashboard.putBoolean("R5", R5);
-        SmartDashboard.putBoolean("R6", R6);
+
+        SmartDashboard.putBoolean("ArmZero", zeroArm.get());
         SmartDashboard.putBoolean("Ball Switch", ballSwitch.get());
 
         SmartDashboard.putBoolean("frontClimbStop", frontClimbStop.get());
@@ -226,106 +223,114 @@ public class Robot extends TimedRobot {
         // 3 - Manual override right
         // 4 - Manual override left
 
-        double CROSS_SLIDE_SPEED = 1.0;
+        // double CROSS_SLIDE_SPEED = 1.0;
 
-        if (rightFace)
-            slideTarget = 1;
-        else if (leftFace)
-            slideTarget = 2;
-        else if (right.getPOV() == 90) 
-            slideTarget = 3;
-        else if (right.getPOV() == 270) 
-            slideTarget = 4;
-        else
-            slideTarget = 0;
+        // if (rightFace)
+        //     slideTarget = 1;
+        // else if (leftFace)
+        //     slideTarget = 2;
+        // else if (right.getPOV() == 90) 
+        //     slideTarget = 3;
+        // else if (right.getPOV() == 270) 
+        //     slideTarget = 4;
+        // else
+        //     slideTarget = 0;
 
-        // So we can find center faster, remember if we hit left or right last
-        if(lSlideSensor.get())
-            lastKnownSlidePosition = 2;
-        else if (rSlideSensor.get())
-            lastKnownSlidePosition = 1;
+        // // So we can find center faster, remember if we hit left or right last
+        // if(lSlideSensor.get())
+        //     lastKnownSlidePosition = 2;
+        // else if (rSlideSensor.get())
+        //     lastKnownSlidePosition = 1;
 
-        if(slideTarget == 1) {
-            // Target is right
-            if(!rSlideSensor.get() || colorTotal < colorThreshhold)
-                crossSlide.set(CROSS_SLIDE_SPEED);
-            else
-                crossSlide.set(0.0); // At right, stop
-        } else if(slideTarget == 2) {
-            // Target is left
-            if(!lSlideSensor.get() || colorTotal < colorThreshhold)
-                crossSlide.set(-CROSS_SLIDE_SPEED);
-            else
-                crossSlide.set(0.0); // At left, stop
-        } else if(slideTarget == 0) {
-            // Stay center
-            if(!cSlideSensor.get()) {
-                if(lastKnownSlidePosition == 1)
-                    crossSlide.set(CROSS_SLIDE_SPEED);
-                else if (lastKnownSlidePosition == 2)
-                    crossSlide.set(-CROSS_SLIDE_SPEED);
-            } else
-                crossSlide.set(0.0); // At center, stop
-        } else if (slideTarget == 3) {
-            // Manual override right
-            if(right.getPOV() == 90 && !rSlideSensor.get())
-                crossSlide.set(CROSS_SLIDE_SPEED);
-            else
-                crossSlide.set(0.0);
-        } else if (slideTarget == 4) {
-            // Manual override left
-            if(right.getPOV() == 270 && !lSlideSensor.get())
-                crossSlide.set(-CROSS_SLIDE_SPEED);
-            else
-                crossSlide.set(0.0);
-        }
+        // if(slideTarget == 1) {
+        //     // Target is right
+        //     if(!rSlideSensor.get() || colorTotal < colorThreshhold)
+        //         crossSlide.set(CROSS_SLIDE_SPEED);
+        //     else
+        //         crossSlide.set(0.0); // At right, stop
+        // } else if(slideTarget == 2) {
+        //     // Target is left
+        //     if(!lSlideSensor.get() || colorTotal < colorThreshhold)
+        //         crossSlide.set(-CROSS_SLIDE_SPEED);
+        //     else
+        //         crossSlide.set(0.0); // At left, stop
+        // } else if(slideTarget == 0) {
+        //     // Stay center
+        //     if(!cSlideSensor.get()) {
+        //         if(lastKnownSlidePosition == 1)
+        //             crossSlide.set(CROSS_SLIDE_SPEED);
+        //         else if (lastKnownSlidePosition == 2)
+        //             crossSlide.set(-CROSS_SLIDE_SPEED);
+        //     } else
+        //         crossSlide.set(0.0); // At center, stop
+        // } else if (slideTarget == 3) {
+        //     // Manual override right
+        //     if(right.getPOV() == 90 && !rSlideSensor.get())
+        //         crossSlide.set(CROSS_SLIDE_SPEED);
+        //     else
+        //         crossSlide.set(0.0);
+        // } else if (slideTarget == 4) {
+        //     // Manual override left
+        //     if(right.getPOV() == 270 && !lSlideSensor.get())
+        //         crossSlide.set(-CROSS_SLIDE_SPEED);
+        //     else
+        //         crossSlide.set(0.0);
+        // }
 
         /*********************************************************
                                 Climb                 
         *********************************************************/
         double downPressure = .2;
-        
-        frontClimb.setInverted(true);
-        rearClimb.setInverted(true);
+
 
         if(L2){
             frontClimb.set(-.5);
+            frontClimb2.set(-.5);
             frontHold = false;
           }       
         else {
             if(L5 && !frontClimbStop.get()){
                 frontClimb.set(.8);
+                frontClimb2.set(.8);
                 frontHold = true;
             }
             else{
                 if(frontHold){
                     frontClimb.set(downPressure);
+                    frontClimb2.set(downPressure);
                 } else{
                     if(frontClimbStop.get()){
                         frontClimb.set(downPressure);
+                        frontClimb2.set(downPressure);
                     } else
                     frontClimb.set(0);
+                    frontClimb2.set(0);
                 }
             }
         }
         
         if(L3){
             rearClimb.set(.5);
-            frontHold = false;
+            rearClimb2.set(.5);
+            rearHold = false;
           }       
         else {
             if(L6 && !rearClimbStop.get()){
                 rearClimb.set(-.8);
-                frontHold = true;
+                rearClimb2.set(-.8);
+                rearHold = true;
             }
             else{
-                if(frontHold){
+                if(rearHold){
                     rearClimb.set(-downPressure);
+                    rearClimb2.set(-downPressure);
                 } else{
                     if(rearClimbStop.get()){
                         rearClimb.set(-downPressure);
+                        rearClimb2.set(-downPressure);
                     } else
                     rearClimb.set(0);
+                    rearClimb2.set(0);
                 }
             }
         }
@@ -353,38 +358,30 @@ public class Robot extends TimedRobot {
         /**********************************************************
                                     LIFT CODE
             *********************************************************/
-        double LIFT_POWER = 0.7;
-        //lLift.setInverted(true);
-        if(right.getPOV() == 0) {
-            lift.set(LIFT_POWER);
-           
-            
+        double LIFT_POWER = 0.4;
+        if(right.getPOV() == 0 && !topLift.get()) {
+            lift.set(1.0);
         } else {
-            if(right.getPOV() == 180) {
+            if(right.getPOV() == 180 && !bottomLift.get())
                 lift.set(-LIFT_POWER);
-                
-               
-            } else {
+            else 
                 lift.set(0.0);
-                
-               
-                
-            }
         }
 
        ////////Intake////////////////
 
-        if(trigger) {
-            lIntake.set(1);
-            rIntake.set(1);
-        } else {
-            if(bottomFace) {
-                lIntake.set(-1);
-                rIntake.set(-1);
-            } else {
-                lIntake.set(0);
-                rIntake.set(0);
-            }
+        if(trigger)
+            if(!ballSwitch.get())
+                intake.set(0.7);
+            else
+                intake.set(0);
+        else {
+            if(bottomFace)
+                intake.set(-1);
+            else if (ballSwitch.get())
+                intake.set(0.0);
+            else
+                intake.set(0);
         }
 
         double ARM_UP_SPEED = 0.8;
@@ -395,28 +392,23 @@ public class Robot extends TimedRobot {
         int SLOWER_TO_HOME = -560;
         int WALL_POS = -560;
         int BALL_PICKUP_POS = -1220;
-        int FLOOR_POS = -1950;
+        int FLOOR_POS = -2100;
 
-        // Home Position
+        //Home Position
         if(R1)
-            armPos = 0;
-        
-        // Ball Pickup Position
-        if(R4)
-            armPos = 2;
-        
+            armPos = 0;        
         // Wall Position
         if(R2)
             armPos = 1;
-
+        // Ball Pickup Position
+        if(R4)
+            armPos = 2;
         // Floor Position
         if(R5)
             armPos = 3;
-        
         // Manual Override Up
         if(R3)
             armPos = 4;
-    
         // Manual Override Down
         if(R6)
             armPos = 5;
@@ -428,6 +420,10 @@ public class Robot extends TimedRobot {
         encoderCount = armEnc.getRaw();
         // double PERC_TO_TOP = (encoderCount / FLOOR_POS);
 
+        if(zeroArm.get()) {
+            armEnc.reset();
+        }
+
         switch(armPos) {
             case 0:
                 if(!zeroArm.get()) {
@@ -437,7 +433,6 @@ public class Robot extends TimedRobot {
                         intakeArm.set(ARM_UP_SPEED);
                     }
                 } else {
-                    armEnc.reset();
                     intakeArm.set(0);
                 }
                 break;
@@ -458,8 +453,7 @@ public class Robot extends TimedRobot {
                 else {
                     if(encoderCount > (BALL_PICKUP_POS + DEAD_ZONE)) {
                         intakeArm.set(ARM_DOWN_SPEED);
-                        lIntake.set(1);
-                        rIntake.set(1);
+                        intake.set(1);
                     } else{
                         intakeArm.set(ARM_HOLD_POWER);
                     }
@@ -477,25 +471,25 @@ public class Robot extends TimedRobot {
                 }
                 break;
             case 4:
-                if(R3) {
-                    if(zeroArm.get()) {
-                        intakeArm.set(0.0);
-                    } else {
-                        intakeArm.set(ARM_UP_SPEED);
-                    }
-                } else {
+                if(zeroArm.get()) {
                     intakeArm.set(0.0);
+                } else {
+                    if(R3) {
+                        intakeArm.set(ARM_UP_SPEED);
+                    } else {
+                        intakeArm.set(ARM_HOLD_POWER);
+                    }
                 }
                 break;
             case 5:
-                if(R6) {
-                    if(encoderCount < (FLOOR_POS - DEAD_ZONE)) {
-                        intakeArm.set(0.0);
-                    } else {
-                        intakeArm.set(ARM_DOWN_SPEED);
-                    }
-                } else {
+                if(encoderCount < (FLOOR_POS - DEAD_ZONE)) {
                     intakeArm.set(0.0);
+                } else {
+                    if(R6) {
+                        intakeArm.set(ARM_DOWN_SPEED);
+                    } else {
+                        intakeArm.set(ARM_HOLD_POWER);
+                    }
                 }
                 break;
             }
@@ -506,19 +500,6 @@ public class Robot extends TimedRobot {
 
         //4063counts per revolution/18.5 gear down = 1 revolution
         //208.79 counts = 1 degree
-
-        if(right.getPOV() == 90)
-            crossSlide.set(.5);
-        else if(right.getPOV() == 270)
-            crossSlide.set(-.5);
-        else
-            crossSlide.set(0);
-
-
-
-
-
-        
     
 
         // OUTPUTS TO SHUFFLEBOARD/SMARTDASHBOARD, USE FOR TRACKING VALUES
@@ -533,7 +514,7 @@ public class Robot extends TimedRobot {
         SmartDashboard.putNumber("target", slideTarget);
         SmartDashboard.putNumber("color total", colorTotal);
         SmartDashboard.putNumber("Arm Encoder", encoderCount);
-        SmartDashboard.putBoolean("Proximity Sensor", cSlideSensor.get());
+        //SmartDashboard.putBoolean("Proximity Sensor", cSlideSensor.get());
         SmartDashboard.putBoolean("High Gear", highGear);
 
         colorSensor.read();
